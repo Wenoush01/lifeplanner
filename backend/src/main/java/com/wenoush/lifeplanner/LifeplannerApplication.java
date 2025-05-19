931d7e55fc91d7b26976b0094e8a5d21888bd44b
Václav <vaclav.slik@gmail.com>
2025-05-19 16:21:26 +0200

    Initial commit

---
+++ backend/src/main/java/com/wenoush/lifeplanner/LifeplannerApplication.java
@@ -0,0 +1,13 @@
+package com.wenoush.lifeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeplannerApplication.class, args);
+	}
+
+}
